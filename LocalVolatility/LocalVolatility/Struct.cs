e2a3a2cb24097525c708fe5b128d77bcf7e6275a
hugoinzirillo <62256540+hugoinzirillo@users.noreply.github.com>
2020-03-20 20:29:35 +0100

    Struct.cs
    This file contains the struct Object : 
    - Option

---
+++ LocalVolatility/LocalVolatility/Struct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetVolStoObjectStruct
+{
+    public struct Option
+    {
+        public double price;
+        public double strike;
+        public double maturity;
+
+    }
+
+
+
+}
+
